05444da4345148c5d71ffff543269699e23e2ad2
justinxlk <justinxlk@gmail.com>
2023-01-25 17:34:34 -0600

    start writing algo

--- robot/algorithm.py
+++ robot/algorithm.py
@@ -1,3 +1,15 @@
 class Algorithm(object):
     def __init__():
         pass
+
+    def turnLeft():
+        #do stuff here
+
+    def turnRight():
+        #do stuff here
+
+    def goStraight():
+        #do stuff here
+
+
+    
