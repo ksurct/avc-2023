4091e3f1ed8f462ee676b6bf0f1171575df96b5b
joCaldwell <joshcaldwell270@gmail.com>
2023-01-25 17:50:21 -0600

    added the old_robot to control the old robot

--- robot/algorithm.py
+++ robot/algorithm.py
@@ -1,3 +1,12 @@
 class Algorithm(object):
-    def __init__():
-        pass
+    def __init__(self):
+        self.turnAngle
+        self.speed
+        self.section = 0
+
+    def run(self):
+        match self.section:
+            case 0:
+                pass
+            case 1:
+                
