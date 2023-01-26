93b36bad58dae484b33e14d34acb95ab1363feee
joCaldwell <joshcaldwell270@gmail.com>
2023-01-25 18:33:47 -0600

    added some base code

--- robot/algorithm.py
+++ robot/algorithm.py
@@ -1,15 +1,52 @@
 class Algorithm(object):
-    def __init__():
+    def __init__(self):
         pass
 
-    def turnLeft():
+    def turnLeft(self):
+        pass
         #do stuff here
 
-    def turnRight():
+    def turnRight(self):
+        pass
         #do stuff here
 
-    def goStraight():
+    def goStraight(self):
+        pass
         #do stuff here
 
-
+class HardCodeAlgo(object):
+    def __init__(self):
+        self.section = 1
+        self.obstacles = ["Blue", "yellow", "ramp", "tunnel"]
+        self.objLastSeen = None
+        
+    def Section1(self):
+        if self.objLastSeen == self.obstacles[0]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
     
+    def Section2(self):
+        pass
+    
+    def Section3(self):
+        pass
+    
+    def Section4(self):
+        pass
+
+    def Section5(self):
+        pass
+
+    def Section6(self):
+        pass
+
+    def Section7(self):
+        pass
+
+    def Section8(self):
+        pass
