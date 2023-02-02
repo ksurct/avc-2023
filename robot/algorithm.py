707a0ac857a94a769bd9a134eecac785c19bd09e
joCaldwell <joshcaldwell270@gmail.com>
2023-02-01 18:45:28 -0600

    Started hard coding the course using logic

--- robot/algorithm.py
+++ robot/algorithm.py
@@ -1,3 +1,4 @@
+#from robot.serialTeensyToPi import SerialInput as SensorData
 class Algorithm(object):
     def __init__(self):
         pass
@@ -15,10 +16,37 @@ class Algorithm(object):
         #do stuff here
 
 class HardCodeAlgo(object):
-    def __init__(self):
+    def __init__(self, sensorData):
         self.section = 1
-        self.obstacles = ["Blue", "yellow", "ramp", "tunnel"]
+        self.obstacles = ["blue", "yellow", "ramp", "tunnel", "end"]
         self.objLastSeen = None
+        self.sensorData = sensorData
+
+        self.distGoneInSec = 0
+        self.angleTurnedInSec = 0
+
+        # To know how far to go in each section feet 
+        self.section1Dist = 20
+        self.section2Dist = 0 #sqrt(30^2+10^2)
+        self.section3Dist = 0 #sqrt(30^2+10^2)
+        self.section4Dist = 18 # the ramp is 2 feet long
+        self.section5Dist = 20 # may need adjusting after landing
+        self.section6Dist = 30
+        self.section7Dist = 30
+        self.section8Dist = 20
+
+        # To know how much to turn at the end of each section degrees
+        self.section1Angle = 0
+        self.section2Angle = 0
+        self.section3Angle = 0
+        self.section4Angle = 0 ## RAMP
+        self.section5Angle = 0
+        self.section6Angle = 0 ## TUNNEL
+        self.section7Angle = 0
+        self.section8Angle = 0 ## END
+        
+    def calculate(self, ):
+        pass
         
     def Section1(self):
         if self.objLastSeen == self.obstacles[0]:
@@ -31,22 +59,65 @@ class HardCodeAlgo(object):
             pass
     
     def Section2(self):
-        pass
+        if self.objLastSeen == self.obstacles[1]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
     
     def Section3(self):
-        pass
+        if self.objLastSeen == self.obstacles[0]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
     
-    def Section4(self):
+    def Section4(self): ## RAMP
         pass
 
     def Section5(self):
-        pass
+        if self.objLastSeen == self.obstacles[0]:
+        
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
 
     def Section6(self):
-        pass
+        if self.objLastSeen == self.obstacles[3]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
 
     def Section7(self):
-        pass
+        if self.objLastSeen == self.obstacles[0]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
 
     def Section8(self):
-        pass
+        if self.objLastSeen == self.obstacles[4]:
+            # sees the correct object
+            # keep going
+            pass
+        else:
+            # sees the wrong object
+            # maybe change to section 2 
+            pass
--- robot/robot.py
+++ robot/robot.py
@@ -3,14 +3,14 @@ from time import time, sleep
 import robot.motor
 from robot.config import Config
 from robot.algorithm import Algorithm
-#from robot.serialTeensyToPi import SerialInput as SensorData
+from robot.serialTeensyToPi import SerialInput as SensorData
 
 #       ALGO, SPEED, CAMERA, TESTING, TIME LIMIT 
 class Robot():
     def __init__(self):
         self.conf = Config(None, 1, False, True, 10, 10)
-        #self.sensorData = SensorData()
-        #self.algo = Algorithm()
+        self.sensorData = SensorData()
+        self.algo = Algorithm(self.sensorData)
     def run(self):
         sleep(self.conf.orientationTime)
         # setup the timeLimit for shutting down the robot after some time
@@ -30,8 +30,10 @@ class Robot():
 
     # this function is called continuously while the robot is running 
     def tick(self):
-        # Run This twice a secondish
+        nextMovement = self.algo.calculate()
+        # for general sensor data
         #self.sensorData.receiveData()
 
+        # for Camera picture
         #self.sensorData.receiveCamData() ## Access with self.sensorData.cam_data
         print("hello")
