25a790dde8b3b04f290805bd1df87d225f755cba
joCaldwell <joshcaldwell270@gmail.com>
2023-03-01 18:20:58 -0600

    more things

--- robot/serialTeensyToPi.py
+++ robot/serialTeensyToPi.py
@@ -2,10 +2,18 @@ import serial as s
 import json
 from time import sleep
 
+# magnetometer, Camera, Encoder, Lidar Sensor?, accelerometer 
 class SerialInput(object):
     
     def __init__(self):
         self.serial = s.Serial(port='/dev/ttyACM0', baudrate=115200)
+
+        # self.mag = [0.0,0.0,0.0,0.0] ## x,y,z,course
+        # self.encoder = 0.0
+        # self.sonar = [-1,-1,-1,-1,-1] ## left, front left, front, front right, right
+        # self.accel = [0.0,0.0,0.0] ## x,y,z
+        self.cam_data = None
+
         self.serial.flush()
         self.fr_data = -1
         self.fl_data = -1
@@ -19,12 +27,24 @@ class SerialInput(object):
         self.magX = 0.0
         self.magY = 0.0
         self.magZ = 0.0
-        self.cam_data = None
 
     def receiveData(self):
         self.pingTeensy()
-        line = json.loads(self.ser.readline().decode('utf-8').rstrip())
+        line = json.loads(self.serial.readline().decode('utf-8').rstrip())
         # print(line) ## DEBUGGING
+#        self.sonar[0] = line['l_data']
+#        self.sonar[0] = line['fl_data']
+#        self.sonar[0] = line['f_data']
+#        self.sonar[0] = line['fr_data']
+#        self.sonar[0] = line['r_data']
+#        self.accel[0] = line['accelX']
+#        self.accel[1] = line['accelY']
+#        self.accel[2] = line['accelZ']
+#        self.mag[0] = line['magX']
+#        self.mag[1] = line['magY']
+#        self.mag[2] = line['magZ']
+#        self.mag[3] = line['magCourse']
+
         self.fr_data = line['fr_data']
         self.fl_data = line['fl_data']
         self.f_data = line['f_data']
@@ -38,14 +58,14 @@ class SerialInput(object):
         self.magY = line['magY']
         self.magZ = line['magZ']
 
-    def recieveCamData(self):
+    def receiveCamData(self):
         pass ## put camera data/picture into self.cam_data
         
     def pingTeensy(self):
         self.serial.write(b'r')
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = s.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
     ser = SerialInput()
 
